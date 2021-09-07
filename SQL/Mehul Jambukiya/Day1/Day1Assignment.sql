2dcdc0c23b4f80e0edb58e4d0e641337fa3a7db7
Mehul Jambukiya <mbjambukiya1305@gmail.com>
2021-09-07 10:16:30 +0530

    REFS:#878 and fixes #878 (Views excercise and added practice file for day 1 to day 6

--- SQL/Mehul Jambukiya/Day1/Day1Assignment.sql
+++ SQL/Mehul Jambukiya/Day1/Day1Assignment.sql
@@ -1,3 +1,10 @@
+
+/*You have been hired to create a relational database to support a car sales business. 
+You need to store information on the business’s employees, inventory, and completed sales.
+You also need to account for the fact that each salesperson receives a different percentage of their 
+sales in commission. What tables and columns would you create in your relational database,
+and how would you link the tables?*/
+
 CREATE DATABASE Assignment1
 
 USE Assignment1
--- SQL/Mehul Jambukiya/Day5/practice.sql
+++ SQL/Mehul Jambukiya/Day5/practice.sql
@@ -1,59 +1,35 @@
+--USE JOINPRACTICE
 
-CREATE DATABASE JOINPRACTICE
+--Joins
+--Inner join
 
-USE JOINPRACTICE
+SELECT * FROM Employee e INNER JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
+SELECT * FROM Employee e JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
 
-CREATE TABLE Employee(
-Employee_Id int CONSTRAINT PkEmpId PRIMARY KEY IDENTITY(1,1),
-First_Name varchar(20) CONSTRAINT FNameNotNull NOT NULL,
-Last_Name varchar(20) CONSTRAINT LNameNotNull NOT NULL,
-Salary money CONSTRAINT SalaryNotNull NOT NULL,
-Joining_Date DATETIME CONSTRAINT JoinDtNotNull NOT NULL,
-Department varchar(30) CONSTRAINT DeptNotNull NOT NULL,
-Manager_Id int CONSTRAINT FkMgrreftoEmp FOREIGN KEY REFERENCES Employee(Employee_Id) ON DELETE NO ACTION ON UPDATE NO ACTION
-)
+--Outer join
+--Left Outer Join
 
-CREATE TABLE Incentives(
-Incentive_Id int CONSTRAINT PkIncentiveId PRIMARY KEY IDENTITY(1,1),
-Employee_Ref_Id int CONSTRAINT FkEmpId FOREIGN KEY REFERENCES Employee(Employee_Id) ON DELETE SET NULL,
-Incentive_Date DATE CONSTRAINT IncentiveDtNotNull NOT NULL,
-Incentive_Amount money CONSTRAINT IncentiveAmountNotNull NOT NULL
-)
-
-INSERT INTO Employee VALUES ('John','Abraham',1000000,'2013-01-01 12:00','Banking',NULL)
-INSERT INTO Employee VALUES ('Michale','Clarke',800000,'2013-01-01 12:00','Insurance',1),
-							('Roy','Thomas',700000,'2013-02-01 12:00','Banking',1),
-							('Tom','Jose',600000,'2013-02-01 12:00','Insurance',2),
-							('Jerry','Pinto',650000,'2013-02-01 12:00','Insurance',3),
-							('Philip','Methew',750000,'2013-01-01 12:00','Services',3),
-							('Testname1','123',650000,'2013-01-01 12:00','Services',2),
-							('Testname2','Lname%',600000,'2013-02-01 12:00','Insurance',2)
-
-INSERT INTO Incentives VALUES (1,'02/01/2013',5000),(2,'02/01/2013',3000),(3,'02/01/2013',4000)
-
-SELECT * FROM Employee
-SELECT * FROM Incentives
+SELECT * FROM Employee e LEFT JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
 
 
---Get difference between JOINING_DATE and INCENTIVE_DATE from employee and incentives table
+--Right Outer join
 
-SELECT e.Joining_Date, i.Incentive_Date, DATEDIFF(DD,e.Joining_Date,i.Incentive_Date) AS [Differnce(in Days)] FROM Employee e JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
+SELECT * FROM Employee e RIGHT JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
 
---Select first_name, incentive amount from employee and incentives table for those employees who have
---incentives and incentive amount greater than 3000
+--Full Outer Join
 
-SELECT e.First_Name, i.Incentive_Amount AS IncentiveAmount FROM Employee e JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id WHERE i.Incentive_Amount > 3000;
+SELECT * FROM Employee e FULL OUTER JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
+SELECT * FROM Employee e FULL JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
 
---Select first_name, incentive amount from employee and incentives table for all employees even if they didn’t get incentives.
 
-SELECT e.First_Name, i.Incentive_Amount AS IncentiveAmount FROM Employee e LEFT JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id 
+--Self-Join
+SELECT * FROM Employee e JOIN Employee m ON e.Employee_Id=m.Employee_Id
+SELECT e.Employee_Id AS employee,e.First_Name as employeename,m.Employee_Id as manager,m.First_Name as managername  
+FROM Employee e LEFT JOIN Employee m ON e.Manager_Id=m.Employee_Id
 
---Select EmployeeName, ManagerName from the employee table.
+--Cross-Join
+SELECT * FROM Employee e CROSS JOIN Incentives i
+SELECT * FROM Incentives i CROSS JOIN Employee e
 
-SELECT e.First_Name AS Employee, m.First_Name AS Manager FROM Employee e LEFT JOIN Employee m ON e.Manager_Id=m.Employee_Id
 
---Select first_name, incentive amount from employee and incentives table for all employees 
---even if they didn’t get incentives and set incentive amount as 0 for those employees who didn’t get incentives.
-
-SELECT e.First_Name, ISNULL(i.Incentive_Amount,0) AS IncentiveAmount FROM Employee e LEFT JOIN Incentives i ON e.Employee_Id=i.Employee_Ref_Id
 
