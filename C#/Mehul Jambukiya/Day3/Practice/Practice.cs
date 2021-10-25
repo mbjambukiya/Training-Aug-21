280c56b6076e2e8d0a902abb5d488e67f9d8ab5d
Mehul Jambukiya <mbjambukiya1305@gmail.com>
2021-10-25 12:36:07 +0530

    REFS:#1840 and fixes #1840 (C# Day3 Assignment and Practice work)

---
+++ C#/Mehul Jambukiya/Day3/Practice/Practice.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    //class Shape
+    //{
+    //    public string Name
+    //    {
+    //        get;
+    //        set;
+    //    }
+
+    //}
+    //class Circle : Shape
+    //{
+    //    public void display()
+    //    {
+    //        Console.WriteLine(Name);
+    //    }
+    //}
+
+    //class Vehical
+    //{
+    //    public virtual void display()
+    //    {
+    //        Console.WriteLine("Inside Vehical Class!");
+    //    }
+    //}
+
+    //class Bike:Vehical
+    //{
+    //    public override void display()
+    //    {
+    //        Console.WriteLine("Inside Bike Class!");
+    //    }
+    //}
+
+    //class Car : Vehical
+    //{
+    //    public override void display()
+    //    {
+    //        Console.WriteLine("Inside Car Class!");
+    //    }
+    //}
+
+    //class Demo
+    //{
+    //    public int x = 5;
+    //    private int y = 5;
+    //}
+
+    //abstract class Vehical
+    //{
+    //    public abstract void display();
+    //}
+
+    //class Bike : Vehical
+    //{
+    //    public override void display()
+    //    {
+    //        Console.WriteLine("diaplay method overrided in bike class...");
+    //    }
+
+    //}
+
+    //class Car : Vehical
+    //{
+    //    public override void display()
+    //    {
+    //        Console.WriteLine("diaplay method overrided in car class...");
+    //    }
+
+    //}
+    class Practice
+    {
+        static void Main(string[] args)
+        {
+            //Inheritance
+
+            //Circle c = new Circle();
+            //c.Name = "Circle";
+            //c.display();
+
+            //Polymorphism
+
+            //Bike b = new Bike();
+            //b.display();
+
+            //Vehical v = new Vehical();
+            //v.display();
+
+            //Vehical v2 = new Bike();
+            //v2.display();
+
+            //Vehical v3 = new Car();
+            //v3.display();
+
+            //Encalsulation
+
+            //Demo d = new Demo();
+            //Console.WriteLine(d.x);
+            //Console.WriteLine(d.y);
+
+            //Absraction
+
+            //Vehical v = new Vehical();
+            //Bike b = new Bike();
+            //b.display();
+            //Car c = new Car();
+            //c.display();
+            //Console.ReadKey();
+        }
+    }
+}
