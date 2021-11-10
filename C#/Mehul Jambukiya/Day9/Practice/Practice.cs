54664f27df23c5143fd6055ecba9bdad9d45ad75
Mehul Jambukiya <mbjambukiya1305@gmail.com>
2021-11-10 18:18:43 +0530

    REFS:#1980 and fixes #1980 (C# day 9 practice work)

---
+++ C#/Mehul Jambukiya/Day9/Practice/Practice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    class Practice
+    {
+        static void Main(string[] args)
+        {
+            method1();
+            method2();
+            method3();
+            Console.ReadKey();
+        }
+        static void method1()
+        {
+            Console.WriteLine("Method1...");
+        }
+        static async Task method2()
+        {
+            await Task.Run(()=>{
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.WriteLine("Method2...");
+                }
+            });
+        }
+        static void method3()
+        {
+            Console.WriteLine("Method3...");
+        }
+
+    }
+}
