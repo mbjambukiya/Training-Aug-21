699af03422882b1cb163883993b550ff1181962d
Mehul Jambukiya <mbjambukiya1305@gmail.com>
2021-11-10 18:14:59 +0530

    REFS:#1912 and fixes #1912(C# day6 assignment and practice work)

---
+++ C#/Mehul Jambukiya/Day6/Assignment/Assignment1.cs
@@ -0,0 +1,26 @@
+﻿//Compute area of rectangle using func delegate
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Assignment1
+    {
+        //public delegate double Func(double l, double w);
+        static double Area(double length, double width)
+        {
+            return length * width;
+        }
+        static void Main(string[] args)
+        {
+            Func<double, double, double> Rectangle = Area;
+            double result = Rectangle(5.5, 4.5);
+            Console.WriteLine($"Area of rectangle is : {result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Mehul Jambukiya/Day6/Assignment/Assignment2.cs
@@ -0,0 +1,22 @@
+﻿//Compute add of two number using lambda expression
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class Assignment2
+    {
+        public delegate int Add(int a, int b);
+        static void Main(string[] args)
+        {
+            Add addition = (a,b) => { return a+b; } ;
+            int result = addition(5, 6);
+            Console.WriteLine($"Addition result is : {result}");
+            Console.ReadKey();
+        }
+    }
+}
